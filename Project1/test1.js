0418a90b848640e81adc5e520448f12ce20db9f8
arsheyas <sardararsheya@gmail.com>
2022-03-22 17:54:19 +0530

    First Commit

---
+++ Project1/test1.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress"/>
+describe('My First Test', function()
+{
+    it('Verify the title of the webpage',function () 
+    {
+        cy.visit("https://espn.insidedisney.com/"),
+        cy.title().should('eq','Login to MyID | Identity And Access Management')
+    })
+  })
